87e3f816e4a89c4fb9129b40b06f9bb685bd2c14
Alex <noto.alex.j@gmail.com>
2016-06-07 05:23:10 +0000

    Completed exercise

--- lib/unique_elements.rb
+++ lib/unique_elements.rb
@@ -2,5 +2,15 @@
 # combination of the above), and returns an array of the elements that appear once and only
 # once.
 def find_unique_elements(arr)
-  
+    totals = Hash.new(0)
+ 	unique = []
+ 	arr.each do |x|
+ 		totals[x] += 1
+ 	end
+	totals.each do |k,v|
+ 		if v == 1
+ 			unique.push(k)
+ 		end
+ 	end
+	return unique
 end
