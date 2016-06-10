9d28561ebd350accbd100a69d63ead0e08515abc
Alex <noto.alex.j@gmail.com>
2016-06-10 05:17:15 +0000

    Added thought process comments

--- lib/unique_elements.rb
+++ lib/unique_elements.rb
@@ -14,3 +14,10 @@ def find_unique_elements(arr)
  	end
 	return unique
 end
+#I approached this exercise the same way I approached the most-frequent-integer exercise. I created a hash so 
+#that I could generate a list of the elements in the given array and also tie to them the number of times
+#they appear. I again iterated over the given array such that the elements from the array are assigned as keys 
+#in the hash, and each time they appear, their value is incremented by one. I then iterated over the hash with
+#an if statement checking for values of one, i.e. elements from the given array that only occured once. If a 
+#value in the hash is equivalent to one, its key is pushed to the 'unique' array. The 'unique' array is returned 
+#at the end of the method.
